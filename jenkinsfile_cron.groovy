a16f5b42f15e07edc4ed38203ab5fc66791a3d90
ravivi <ravivi@cisco.com>
2023-03-29 12:04:01 +0300

    add aws creds

--- jenkinsfile_cron.groovy
+++ jenkinsfile_cron.groovy
@@ -19,7 +19,7 @@ pipeline {
                     accessKeyVariable: 'AWS_ACCESS_KEY_ID',
                     secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
                 ]])
-                script{
+                {
                     export AWS_REGION='eu-central-1'
                     sh "docker run -it roie710/${params.DOCKER_IMAGE_TAG}:${params.IMAGE_VERSION}"
                 }

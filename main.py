760e024011511bd96ce0af1ea06e204b64af0139
ravivi <ravivi@cisco.com>
2023-03-29 15:03:32 +0300

    add logger support

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import boto3
+import logging
+from pythonjsonlogger import jsonlogger
 
+logger = logging.getLogger()
+
+logHandler = logging.StreamHandler()
+formatter = jsonlogger.JsonFormatter()
+logHandler.setFormatter(formatter)
+logger.addHandler(logHandler)
+
+ec2 = boto3.client('ec2')
 
 def list_running_instances():
     try:
@@ -13,29 +23,18 @@ def list_running_instances():
             ]
         )
 
-        ## Print instance information
+        ## Log instance information
         for reservation in instances['Reservations']:
             for instance in reservation['Instances']:
-                print(f"Instance ID: {instance['InstanceId']}")
-                print(f"Instance Type: {instance['InstanceType']}")
-                print(f"Public IP Address: {instance.get('PublicIpAddress', 'N/A')}")
-                print(f"Private IP Address: {instance['PrivateIpAddress']}")
-                print(f"Availability Zone: {instance['Placement']['AvailabilityZone']}")
-                print(f"State: {instance['State']['Name']}")
-
-                # Print instance tags
-                tags = instance.get('Tags', [])
-                if tags:
-                    print("Tags:")
-                    for tag in tags:
-                        print(f"- {tag['Key']}: {tag['Value']}")
-
-                print("---------------------------")
+                logger.info({
+                    "instance_id": instance['InstanceId'],
+                    "instance_type": instance['InstanceType'],
+                    "public_ip_address": instance.get('PublicIpAddress', 'N/A'),
+                    "private_ip_address": instance['PrivateIpAddress'],
+                    "availability_zone": instance['Placement']['AvailabilityZone'],
+                    "state": instance['State']['Name'],
+                    "tags": instance.get('Tags', [])
+                })
 
     except Exception as e:
-        print(f"Error: {str(e)}")
-
-
-if __name__ == "__main__":
-    ec2 = boto3.client('ec2')
-    list_running_instances()
+        logger.error(e)

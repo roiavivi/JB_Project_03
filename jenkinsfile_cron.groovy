8ceb260ad1229bc94693e32aaad5bada70113aad
ravivi <ravivi@cisco.com>
2023-03-29 10:14:38 +0300

    create

---
+++ jenkinsfile_cron.groovy
@@ -0,0 +1,22 @@
+pipeline {
+    agent any
+    parameters {
+        string(name: 'BRANCH', defaultValue: 'main', description: 'The branch to checkout from GitHub')
+        string(name: 'DOCKER_IMAGE_TAG', defaultValue: 'devops-integration', description: 'The tag to use for the Docker image')
+        string(name: 'BUILD_NUMBER', defaultValue: 'latest', description: 'The version for the Docker image')
+    }
+    stages {
+        stage('Pull from GitHub') {
+            steps {
+                checkout([$class: 'GitSCM', branches: [[name: "${params.BRANCH}"]], userRemoteConfigs: [[url: 'https://github.com/roiavivi/JB_Project_03.git']]])
+            }
+        }
+        stage('Pull Docker image') {
+            steps {
+                script {
+                    docker.pull("roie710/${params.DOCKER_IMAGE_TAG}:${BUILD_NUMBER}")
+                }
+            }
+        }
+    }
+}

3537efac09626c83050cf4f665386aebb9fc3ab8
ravivi <ravivi@cisco.com>
2023-03-29 12:12:18 +0300

    add aws creds

--- jenkinsfile_cron.groovy
+++ jenkinsfile_cron.groovy
@@ -1,5 +1,8 @@
 pipeline {
     agent any
+    environment {
+        AWS_REGION = 'eu-central-1'
+    }
     parameters {
         string(name: 'BRANCH', defaultValue: 'main', description: 'The branch to checkout from GitHub')
         string(name: 'DOCKER_IMAGE_TAG', defaultValue: 'devops-integration', description: 'The tag to use for the Docker image')
@@ -8,19 +11,18 @@ pipeline {
     stages {
         stage('Pull from GitHub') {
             steps {
-                checkout([$class: 'GitSCM', branches: [[name: "${params.BRANCH}"]], userRemoteConfigs: [[url: 'https://github.com/roiavivi/JB_Project_03.git']]])
+                git branch: "${params.BRANCH}", url: 'https://github.com/roiavivi/JB_Project_03.git'
             }
         }
         stage('Run Docker image') {
             steps {
-                withCredentials([[
-                    $class: 'AmazonWebServicesCredentialsBinding',
-                    credentialsId: "aws-jenkins-demo",
-                    accessKeyVariable: 'AWS_ACCESS_KEY_ID',
-                    secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
-                ]])
-                {
-                    export AWS_REGION='eu-central-1'
+                withCredentials([
+                    [ $class: 'AmazonWebServicesCredentialsBinding',
+                      credentialsId: 'aws-jenkins-demo',
+                      accessKeyVariable: 'AWS_ACCESS_KEY_ID',
+                      secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
+                    ]
+                ]) {
                     sh "docker run -it roie710/${params.DOCKER_IMAGE_TAG}:${params.IMAGE_VERSION}"
                 }
             }

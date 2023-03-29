b0e0397cebf07eb8860f7fef86752c8b5d1aefee
ravivi <ravivi@cisco.com>
2023-03-29 18:11:11 +0300

    get CI last build

--- jenkinsfile_cron.groovy
+++ jenkinsfile_cron.groovy
@@ -27,7 +27,7 @@ pipeline {
                         def jobName = 'CI'
                         def job = Jenkins.instance.getItemByFullName(jobName)
                         def lastCIBuild = job.getLastSuccessfulBuild()
-                        sh "docker run --env AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION} --env AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} --env AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}  roie710/${params.DOCKER_IMAGE_TAG}:${${lastCIBuild.number}"
+                        sh "docker run --env AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION} --env AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} --env AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}  roie710/${params.DOCKER_IMAGE_TAG}:${lastCIBuild.number}"
                     }
                 }
             }

7f2c15a03772a6c8981453cadf0cfd741c8cfcae
ravivi <ravivi@cisco.com>
2023-03-30 09:49:14 +0300

    add pylint

--- main.py
+++ main.py
@@ -37,8 +37,7 @@ def list_running_instances():
                         instance.get('PublicIpAddress', 'N/A'),
                     "private_ip_address":
                         instance['PrivateIpAddress'],
-                    "availability_zone":
-                        instance['Placement']['AvailabilityZone'],
+                    "availability_zone": instance['Placement']['AvailabilityZone'],
                     "state":
                         instance['State']['Name'],
                     "tags":

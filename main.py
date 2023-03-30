be61faef56d1715a5f16515cc07a80621772bb1d
ravivi <ravivi@cisco.com>
2023-03-30 16:05:34 +0300

    fix docker compose

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import logging
 import boto3
+from pythonjsonlogger import jsonlogger
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 logHandler = logging.StreamHandler()
-formatter = logging.Formatter\
-    ('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = jsonlogger.JsonFormatter()
 logHandler.setFormatter(formatter)
 logger.addHandler(logHandler)
+
 
 ec2 = boto3.client('ec2')
 
@@ -27,17 +28,22 @@ def list_running_instances():
         ## Log instance information
         for reservation in instances['Reservations']:
             for instance in reservation['Instances']:
-                logger.info(
-                    "Instance ID: %s, Type: %s, Public IP: %s, "
-                    "Private IP: %s, Zone: %s, State: %s, Tags: %s",
-                    instance['InstanceId'],
-                    instance['InstanceType'],
-                    instance.get('PublicIpAddress', 'N/A'),
-                    instance['PrivateIpAddress'],
-                    instance['Placement']['AvailabilityZone'],
-                    instance['State']['Name'],
-                    instance.get('Tags', [])
-                )
+                logger.info({
+                    "instance_id":
+                        instance['InstanceId'],
+                    "instance_type":
+                        instance['InstanceType'],
+                    "public_ip_address":
+                        instance.get('PublicIpAddress', 'N/A'),
+                    "private_ip_address":
+                        instance['PrivateIpAddress'],
+                    "availability_zone":
+                        instance['Placement']['AvailabilityZone'],
+                    "state":
+                        instance['State']['Name'],
+                    "tags":
+                        instance.get('Tags', [])
+                })
 
     except Exception as e:
         logger.error(e)
